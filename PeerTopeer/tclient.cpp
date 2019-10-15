21963c144e87d865b1e21cdc7f806bb97c157fdd
Anvesh <anveshrambo7@gmail.com>
2019-10-15 23:29:58 +0530

    Final Commit

---
+++ PeerTopeer/tclient.cpp
@@ -0,0 +1,544 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sstream>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <openssl/sha.h>
+#include<sys/sendfile.h>
+using namespace std;
+
+string command[100];
+int command_index;
+int connfd[100];
+string userName="";
+string groupOwner="";
+pthread_t clientThread;
+char destPath[50];
+
+struct file
+{
+	string name;
+	string gid;
+	string hashValue;
+	int fileSize;
+	string userID;
+};
+struct file f;
+struct file newFile()
+{
+	struct file f;
+	f.name="";
+	f.gid="";
+	f.hashValue="";
+	f.fileSize =0;
+	f.userID = "";
+	return f;
+}
+void error (const char *msg)
+{
+	perror (msg);
+	exit(1);
+}
+
+string extractPortNumber(string userID)
+{
+	int fd=open("Tracker.txt",O_RDWR,0666);
+	int flag=0;
+	string u="";
+	string p="";
+	char c;
+	while(1)
+	{
+		u="";
+		p="";
+		while(read(fd,&c,1)>0)
+		{
+			if(c!='-')
+				u+=c;
+			else
+				break;
+		}
+		if(userID==u)
+		{
+			while(read(fd,&c,1)>0)
+			{
+				if(c!='\n')
+					p+=c;
+				else
+					break;
+			}
+			break;
+		}
+		else
+			while(read(fd,&c,1)>0)
+				if(c=='\n')
+					break;
+	}
+	// cout<<p<<endl;
+	close(fd);
+	return p;
+}
+void commandTokenizer(string input_command)
+{
+	int i,j,k=0;
+	// cout<<"Received in tokenizer "<<input_command<<endl;
+	command_index=0;
+	for(i=0; i<input_command.length(); )
+	{
+		string s="";
+		for(j=i; j<input_command.length(); j++)
+		{
+			if(input_command[j]!=' ' && input_command[j]!='\n')
+			{
+				s=s+input_command[j];			
+			}
+			else
+				break;
+		}
+		command[command_index++] = s;
+		i=j+1;
+	}
+}
+void *clientClient(void *arg)
+{
+	int sockfd , portno, n;
+	struct sockaddr_in server_address;
+	
+	struct hostent *server;
+
+	char recvBuffer[2048];
+	char user[255];
+	char pass[255];
+	
+	cout<<"User id of Client requesting file is"<<f.userID<<endl;
+	string p = extractPortNumber(f.userID);
+	// cout<<"Port number in string  is : "<<p;
+	// portno = atoi(p.c_str());
+	
+	portno = atoi(p.c_str());
+	// cout<<"Port number in integer is : "<<p;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(sockfd < 0)
+		error("ERROR opening socket");
+
+	server = gethostbyname("127.0.0.1");
+	if(server == NULL)
+	{
+		fprintf(stderr, "Error,no such hosts\n");
+		pthread_exit(NULL);
+	}
+
+	bzero((char *)&server_address, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,(char *)&server_address.sin_addr.s_addr,server->h_length);
+	server_address.sin_port = htons(portno);
+
+	if(connect(sockfd,(struct sockaddr *)&server_address,sizeof(server_address))<0)
+	{
+		error("Connection failed in Client\n");
+		pthread_exit(NULL);
+	}
+
+	char fn[100];
+
+	strcpy(fn,f.name.c_str());
+	// cout<<"File to be copied is "<<t<<endl;
+	
+	send(sockfd,fn,100,0);
+	
+	int fs=0;
+	
+	
+	recv(sockfd, &(fs), sizeof(int),0);
+	// cout<<"Server sent filesize "<<size1<<endl;
+	
+	char filebuffer[fs];
+	recv(sockfd,filebuffer,fs,0);
+	
+	int fd=open(destPath,O_RDWR|O_CREAT,0666);
+	
+	write(fd,filebuffer,fs);
+	cout<<"File is Succefully Downloaded\n";
+	close(fd);
+	close(sockfd);
+
+}
+void *clientServer(void *arg)
+{
+	int i=0,ci=0;
+	// int sockfd = 0;
+	struct stat f;
+	int fsize;
+	
+	cout<<"Server of client called\n";
+	int sockfd, newsockfd, portno,n;
+	struct sockaddr_in server_address, client_address;
+	socklen_t clilen;
+	
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(sockfd < 0)
+	{
+		error("Error opening socket");
+	}
+
+	bzero((char *)&server_address, sizeof(server_address));
+	cout<<"User id of Client requesting file is"<<userName<<endl;
+	
+	string p = extractPortNumber(userName);
+	
+	// cout<<"Port number in string  is : "<<p;
+	portno = atoi(p.c_str());
+	// cout<<"Port number in integer is : "<<p;
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_port = htons(portno);	
+
+	if(bind(sockfd , (struct sockaddr *) &server_address,sizeof(server_address)) < 0)
+		error("Binding Failed in server of client");
+
+	listen(sockfd, 5);
+	clilen = sizeof(client_address);
+
+	int conid = accept(sockfd, (struct sockaddr*)NULL, NULL); 
+	char filePath[50];		
+	recv(conid,filePath,50,0);
+	cout<<"client sent file "<<filePath<<endl;
+	
+	stat(filePath,&f);
+	fsize=f.st_size;
+	
+	cout<<"Size of file is "<<fsize<<endl;
+	send(conid, &fsize, sizeof(int),0);
+	int fd=open(filePath,O_RDWR,0666);
+	cout<<"file opened \n";
+	sendfile(conid,fd,NULL,fsize);
+	cout<<"file Sent Succefully\n";
+	close(fd);
+	close(conid);
+		
+}
+
+int main(int argc, char *argv[])
+{
+	// pthread_attr_t custom1,custom2,custom3;
+	// pthread_attr_init(&custom1);
+	// pthread_attr_init(&custom2);
+	// // pthread_attr_init(&custom3);
+
+	int sockfd , portno, n;
+	struct sockaddr_in server_address;
+	
+	struct hostent *server;
+
+	char recvBuffer[2048];
+	char user[255];
+	char pass[255];
+	
+	if(argc < 3)
+	{
+		fprintf(stderr, "usage %s hostname port\n", argv[0]);
+		exit(1);
+	}
+
+	portno = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(sockfd < 0)
+		error("ERROR opening socket");
+
+	server = gethostbyname(argv[1]);
+	if(server == NULL)
+	{
+		fprintf(stderr, "Error,no such hosts\n");
+	}
+
+	bzero((char *)&server_address, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,(char *)&server_address.sin_addr.s_addr,server->h_length);
+	server_address.sin_port = htons(portno);
+
+	if(connect(sockfd,(struct sockaddr *)&server_address,sizeof(server_address))<0)
+		error("Connection failed\n");
+
+	while(1)
+	{
+		bzero(user,255);
+		bzero(pass,255);
+		bzero(recvBuffer,2048);
+		// bzero(buffer,255);
+		int i,j,k=0;
+		string input_command="";
+		fflush(stdin);
+		// fflush(stdout);
+		getline(cin,input_command);
+
+		commandTokenizer(input_command);
+		cout<<command[1]<<" "<<command[2]<<endl;
+		
+		if(command[0]=="cu")
+		{
+			fflush(stdout);
+			write(sockfd,"cu",3);
+			
+			// string u&p = command[1]+" "+command[2];
+			
+			strcpy(user,command[1].c_str());
+			strcpy(pass,command[2].c_str());
+			
+			write(sockfd,user,255);
+			write(sockfd,pass,255);
+
+			read(sockfd,recvBuffer,255);
+			printf("Server: %s",recvBuffer);
+
+		}
+		else if(command[0]=="login")
+		{
+			
+			write(sockfd,"login",6);
+			
+			// string u&p = command[1]+" "+command[2];
+			
+			strcpy(user,command[1].c_str());
+			strcpy(pass,command[2].c_str());
+			
+			write(sockfd,user,255);
+			write(sockfd,pass,255);
+			
+			bzero(recvBuffer,255);
+			read(sockfd,recvBuffer,255);
+			// cout<<"Serever sent for login: "<<recvBuffer;
+			if(strcmp(recvBuffer,"1")==0)
+			{
+				groupOwner = command[1];
+				bzero(recvBuffer,255);
+				read(sockfd,recvBuffer,255);
+				printf("Server: %s",recvBuffer);
+				pthread_t newServer;
+				userName = command[1];
+				// cout<<""<<userName<<endl;
+				pthread_create(&newServer,NULL,&clientServer,NULL);
+			}
+			else
+			{
+				bzero(recvBuffer,255);
+				read(sockfd,recvBuffer,255);
+				printf("Server: %s",recvBuffer);	
+			}
+		}
+		else if(command[0]=="cg")
+		{
+			char groupOwn[255];
+			char groupId[10];
+			
+			write(sockfd,"cg",3);
+			// string groupInfo = command[1]+"-"+groupOwner;
+			strcpy(groupOwn,groupOwner.c_str());
+			strcpy(groupId,command[1].c_str());
+			
+			write(sockfd,groupId,10);
+			write(sockfd,groupOwn,255);
+			
+			bzero(recvBuffer,255);
+			read(sockfd,recvBuffer,255);
+			printf("Server: %s",recvBuffer);
+		}
+		else if(command[0]=="jg")
+		{
+			char groupId[10];
+			
+			write(sockfd,"jg",3);
+
+			strcpy(groupId,command[1].c_str());
+
+			write(sockfd,groupId,10);
+			bzero(recvBuffer,255);
+			read(sockfd,recvBuffer,255);
+			printf("Server: %s", recvBuffer);
+
+		}
+		else if(command[0]=="ar")
+		{
+			char groupId[10];
+			char userID[20];
+			write(sockfd,"ar",3);
+
+			strcpy(groupId,command[1].c_str());
+			strcpy(userID,command[2].c_str());
+			
+			write(sockfd,groupId,10);
+			write(sockfd,userID,20);
+			
+			bzero(recvBuffer,255);
+			read(sockfd,recvBuffer,255);
+			printf("Server : %s",recvBuffer);
+		}
+		else if(command[0]=="lig")
+		{
+			char numberOfGroups[20];
+			write(sockfd,"lg",3);
+			
+			bzero(recvBuffer,255);
+			
+			read(sockfd,recvBuffer,2048);
+			
+			printf("%s",recvBuffer);
+
+		}
+		else if(command[0]=="leg")
+		{
+			char groupId[10];
+			write(sockfd,"leg",5);
+
+			strcpy(groupId,command[1].c_str());
+						
+			write(sockfd,groupId,10);
+
+			bzero(recvBuffer,2048);
+			read(sockfd,recvBuffer,255);
+			printf("Server : %s",recvBuffer);
+
+		}
+		else if(command[0]=="lir")
+		{
+			char groupId[10];
+			write(sockfd,"lir",4);
+			
+			strcpy(groupId,command[1].c_str());
+
+			write(sockfd,groupId,10);
+			
+			bzero(recvBuffer,255);
+			read(sockfd,recvBuffer,2048);
+			printf("%s",recvBuffer);
+		}
+		else if(command[0]=="uf")
+		{
+			char file[20];
+			char groupId[10];
+			write(sockfd,"uf",3);
+			
+
+			strcpy(file,command[1].c_str());
+			strcpy(groupId,command[2].c_str());
+			
+			cout<<"filename is "<<file<<endl;
+			
+			struct stat fileName;
+			int fileSize;
+			
+			stat(file,&fileName);
+			cout<<"Stat Executed\n";
+			fileSize=fileName.st_size;
+			int filesize = fileSize;
+			fileSize=min(fileSize,20);
+
+			// char shaBuff[fileSize];
+
+ 			unsigned char *filebuff;
+ 			char hsh[3];
+ 			filebuff = (unsigned char *)malloc (fileSize);
+			int fd = open(file,O_RDWR,0666);
+			cout<<"File opened Succefully.\n";
+			
+			read(fd,filebuff,fileSize);
+			cout<<"File Read.\n";
+			cout<<filebuff<<endl;
+			close(fd);
+			
+			string hashValue = "";
+			
+			unsigned char shaBuff[SHA_DIGEST_LENGTH];
+			const unsigned char *f=filebuff;
+
+			SHA1(f, fileSize, shaBuff);
+			cout<<"hash computed"<<endl;
+			
+			for(int i = 0; i < SHA_DIGEST_LENGTH; i++)
+			{
+				sprintf(hsh,"%02x",shaBuff[i]);
+				hashValue.append(hsh);
+			}
+			
+			cout<<"Hash is "<<hashValue<<endl;
+			string fileInfo = "";
+			fileInfo = command[1]+" "+command[2]+" "+ hashValue +" "+to_string(filesize);
+			cout<<fileInfo<<endl;
+			write(sockfd,fileInfo.c_str(),strlen(fileInfo.c_str()));
+
+			bzero(recvBuffer,2048);
+			read(sockfd,recvBuffer,255);
+			printf("Server : %s",recvBuffer);
+		}
+		else if(command[0]=="df")
+		{
+			char gId[10];
+			char fileName[50];
+			
+			
+			char sha[100];
+			char fileO[50];
+			int sizeOfFile;
+
+			char found[2];
+
+			write(sockfd,"df",3);
+
+			strcpy(gId,command[1].c_str());
+			strcpy(fileName,command[2].c_str());
+			strcpy(destPath,command[3].c_str());
+			cout<<"destination Path is "<<destPath<<endl;
+
+			write(sockfd,gId,10);
+			write(sockfd,fileName,50);
+
+			// read(sockfd,found,1);
+			// cout<<"server sent "<<found<<endl;
+			// if(strcmp(found,"1")==0)
+			{
+				bzero(fileO,50);
+				read(sockfd,fileO,50);
+				cout<<"file Owner is "<<fileO<<endl;
+				read(sockfd,&sizeOfFile,sizeof(int));
+				read(sockfd,sha,100);
+				
+				f = newFile();
+
+				f.name=string(fileName);
+				f.gid=string(gId);
+				f.hashValue=string(sha);
+				f.fileSize =sizeOfFile;
+				f.userID=string(fileO);
+				cout<<"file Owner is "<<f.userID<<endl;
+				cout<<"file Info "<<f.name<<" "<<f.gid<<" "<<" "<<f.hashValue<<" "<<f.userID<<endl;
+				pthread_create(&clientThread,NULL,&clientClient,NULL);
+				pthread_join(clientThread,NULL);
+
+			}
+			// else
+			// {
+			// 	bzero(recvBuffer,2048);
+			// 	read(sockfd,recvBuffer,255);
+			// 	printf("Server : %s",recvBuffer);
+			// }
+			// fflush(stdin);
+		}
+		else
+		{
+			cout<<"Enter a valid Command\n";
+		}
+	}
+	return 0;
+}
--- PeerTopeer/tserver.cpp
+++ PeerTopeer/tserver.cpp
@@ -9,7 +9,7 @@
 #include <pthread.h>
 #include <map>
 #include <string>
-#include<fcntl.h>
+#include <fcntl.h>
 #include <vector>
 
 using namespace std;
@@ -20,6 +20,7 @@ struct file
 	string gid;
 	string hashValue;
 	int fileSize;
+	string userID;
 };
 
 pthread_t thread[10];
@@ -48,6 +49,8 @@ struct file newFile()
 	f.gid="";
 	f.hashValue="";
 	f.fileSize =0;
+	f.userID = "";
+	return f;
 }
 
 int loggedIn[100]={0};
@@ -75,8 +78,8 @@ void createNewUser(string username,string password,int id)
 {
 	char suc[5] = "hi\n";
 	map<string,string>::iterator it;
-	cout<<"Entered createUser function"<<endl;
-	cout<<"Parameters are :"<<username<<" and "<<password<<endl;
+	// cout<<"Entered createUser function"<<endl;
+	// cout<<"Parameters are :"<<username<<" and "<<password<<endl;
 	// cout<<"fdsaasdfas\n";
 	for(it=user.begin(); it!=user.end(); it++)
 	{
@@ -122,8 +125,8 @@ void displayUsers(int id)
 
 void loginUser(string username,string password,int id)
 {
-	cout<<"Entered login function"<<endl;
-	cout<<"Parameters are :"<<username<<" and "<<password<<endl;
+	// cout<<"Entered login function"<<endl;
+	// cout<<"Parameters are :"<<username<<" and "<<password<<endl;
 	map<string,string>::iterator it;
 	
 	for(it=user.begin(); it!=user.end(); it++)
@@ -133,12 +136,16 @@ void loginUser(string username,string password,int id)
 			loggedIn[id]=1;
 			connIdAndUser[to_string(connfd[id])] = username;
 			fflush(stdout);
+			write(connfd[id],"1",2);
+			fflush(stdout);
 			write(connfd[id],"User successfully logged in.\n",strlen("User successfully logged in.\n"));
 			// cout<<"User successfully logged in"<<endl;
 			return ;
 		}
 	}
 
+	fflush(stdout);
+	write(connfd[id],"0",2);
 	fflush(stdout);
 	write(connfd[id],"Login Failed!\n",strlen("Login Failed!\n"));
 	// cout<<"Login Failed\n";
@@ -157,7 +164,9 @@ void *readd(void *arg)
   	char* msg[50];
 	while(1)
 	{
+		fflush(stdout);
 		bzero(buffer , 255);
+
 		n = read(connfd[*id],buffer,255);
 		
 		if(buffer[0]=='\0')
@@ -170,7 +179,7 @@ void *readd(void *arg)
 		
 		if(strcmp(buffer,"cu")==0)
 		{
-			cout<<"Create User called\n";
+			// cout<<"Create User called\n";
 			char user[255];
 			char pass[255];
 			read(connfd[*id],user,255);
@@ -179,7 +188,7 @@ void *readd(void *arg)
 		}
 		else if(strcmp(buffer,"login")==0)
 		{
-			cout<<"Login Function Called\n";
+			// cout<<"Login Function Called\n";
 			char user[255];
 			char pass[255];
 			
@@ -190,7 +199,7 @@ void *readd(void *arg)
 		}
 		else if(strcmp(buffer,"cg")==0)
 		{
-			cout<<"Create Group Called\n";
+			// cout<<"Create Group Called\n";
 			if(loggedIn[*id]==1)
 			{
 				char groupOwn[255];
@@ -206,16 +215,24 @@ void *readd(void *arg)
 						group[string(groupId)] = string(groupOwn);
 						write(connfd[*id],"Group successfully created.\n",strlen("Group successfully created.\n"));
 						groupUsers.push_back(make_pair(string(groupId),string(groupOwn)));
-						int fd=open("groupInfo.txt",O_RDWR|O_CREAT|O_APPEND,0666);
-	
-						write(fd,groupId,strlen(groupId));
-						write(fd,":",1);
-						string grOwn(groupOwn);
-						groupOwn[grOwn.length()]='\n';
-						groupOwn[grOwn.length()+1]='\0';
 						
-						write(fd,groupOwn,strlen(groupOwn)+1);
+						int fd=open("groupInfo.txt",O_RDWR|O_CREAT|O_APPEND,0666);
+						string gi="";
+						gi+=string(groupId)+"-"+string(groupOwn)+"\n";
+						char wr[50];
+						strcpy(wr,gi.c_str());
+						write(fd,wr,strlen(wr)+1);
 						close(fd);
+
+						char t[10];
+						strcpy(t,groupOwn);
+						int fd1=open(t,O_RDWR|O_CREAT|O_APPEND,0666);
+						gi="";
+						gi+=string(groupId)+"-"+string(groupOwn)+"\n";
+						char wr1[50];
+						strcpy(wr1,gi.c_str());
+						write(fd1,wr1,strlen(wr1)+1);
+						close(fd1);
 					}
 				else
 					{
@@ -223,15 +240,21 @@ void *readd(void *arg)
 						write(connfd[*id],"Group already exists.\n",strlen("Group already exists.\n"));			
 					}
 			}
+			else
+			{
+				fflush(stdout);
+				write(connfd[*id],"You are not logged in.Please login first.\n",strlen("You are not logged in.Please login first.\n"));
+				continue;
+			}
 		}
 		else if(strcmp(buffer,"jg")==0)
 		{
-			// if(loggedIn[*id]==1)
+			if(loggedIn[*id]==1)
 			{
-				cout<<"Join group called\n";
+				// cout<<"Join group called\n";
 				char groupId[10];
 				read(connfd[*id],groupId,10);
-				cout<<"request for : "<<groupId<<endl;
+				// cout<<"request for : "<<groupId<<endl;
 				string gid(groupId);
 				auto it = group.find(gid);
 				// map<string,string>::iterator itj;
@@ -244,7 +267,7 @@ void *readd(void *arg)
 					strcpy(p,(it->second).c_str());
 					auto it1=connIdAndUser.find(to_string(connfd[*id]));
 					requestList.push_back(make_pair(it->first,it1->second));
-					cout<<"Request is Successful\n";
+					// cout<<"Request is Successful\n";
 					fflush(stdout);	
 					write(connfd[*id],"Request is Pending.\n",strlen("Request is Pending.\n"));
 				}
@@ -254,13 +277,19 @@ void *readd(void *arg)
 					write(connfd[*id],"Group does not exist.\n",strlen("Group does not exist.\n"));
 				}	
 			}
+			else
+			{
+				fflush(stdout);
+				write(connfd[*id],"You are not logged in.Please login first.\n",strlen("You are not logged in.Please login first.\n"));
+				continue;
+			}
 			
 		}
 		else if(strcmp(buffer,"ar")==0)
 		{
 			if(loggedIn[*id]==1)
 			{
-				cout<<"Accept Request is called\n";
+				// cout<<"Accept Request is called\n";
 				char groupId[10];
 				char userId[10];
 
@@ -292,75 +321,242 @@ void *readd(void *arg)
 				// cout<<"Connection id of requseted user is: "<<conID<<endl;
 				requestList.erase(it);
 
+				auto it4=group.find(to_string(connfd[*id]));
+				string s1="";
+				char go[50];
+				strcpy(go,it4->second.c_str());
+				s1+=string(groupId)+"-"+string(userId)+"\n";
+				int fd=open(go,O_RDWR|O_CREAT|O_APPEND,0666);
+				char wr1[50];
+				strcpy(wr1,s1.c_str());
+				write(fd,wr1,strlen(wr1)+1);
 				fflush(stdout);
 				write(connfd[*id],"Request is accepted.\n",strlen("Request is accepted.\n"));		
+			}
+			else
+			{
+				fflush(stdout);
+				write(connfd[*id],"You are not logged in.Please login first.\n",strlen("You are not logged in.Please login first.\n"));
+				continue;
 			}
 		}
 		else if(strcmp(buffer,"lg")==0)
 		{
-			char groupsList[2048];
-			cout<<"List groups called\n";
-			// string numberOfGroups = to_string(noOfGroups);
-			// // cout<<"total Groups in string are :"<<numberOfGroups<<endl;
-			// write(connfd[*id],numberOfGroups.c_str(),strlen(numberOfGroups.c_str()));
-			string gl="";
-			
-			for(auto it=group.begin(); it!=group.end(); it++)
+			if(loggedIn[*id]==1)
 			{
-				gl=gl+it->first+" "+it->second+"\n";
+				char groupsList[2048];
+				// cout<<"List groups called\n";
+				// string numberOfGroups = to_string(noOfGroups);
+				// // cout<<"total Groups in string are :"<<numberOfGroups<<endl;
+				// write(connfd[*id],numberOfGroups.c_str(),strlen(numberOfGroups.c_str()));
+				string gl="Following Groups are created \n";
+				int flag=0;
+				for(auto it=group.begin(); it!=group.end(); it++)
+				{
+					gl=gl+it->first+" "+it->second+"\n";
+					flag=1;
+				}
+				if(flag==1)
+				{
+					bzero(groupsList,2048);
+					strcpy(groupsList,gl.c_str());
+					fflush(stdout);
+					write(connfd[*id],groupsList,strlen(gl.c_str()));
+				}
+				else
+				{
+					fflush(stdout);
+					write(connfd[*id],"No groups are created till now.\n",strlen("No groups are created till now.\n"));
+				}
 			}
-			bzero(groupsList,2048);
-			strcpy(groupsList,gl.c_str());
-			fflush(stdout);
-			write(connfd[*id],groupsList,strlen(gl.c_str()));
+			else
+			{
+				fflush(stdout);
+				write(connfd[*id],"You are not logged in.Please login first.\n",strlen("You are not logged in.Please login first.\n"));
+				continue;
+			}
+		}
+		else if(strcmp(buffer,"lir")==0)
+		{
+			if(loggedIn[*id]==1)
+			{	
+				char requestListBuff[2048];
+				char groupId[10];
+				// cout<<"List request called\n";
+				read(connfd[*id],groupId,10);
+				// string numberOfGroups = to_string(noOfGroups);
+				// // cout<<"total Groups in string are :"<<numberOfGroups<<endl;
+				// write(connfd[*id],numberOfGroups.c_str(),strlen(numberOfGroups.c_str()));
+				string gId(groupId);
+				string rl="following users requseted for your group \n";
 
+				int flag=0;
+				for(auto it=requestList.begin(); it!=requestList.end(); it++)
+				{
+					if(it->first==gId)
+					{
+						rl=rl+it->second+"\n";
+						flag=1;
+					}
+				}
+				if(flag==1)
+				{	
+					bzero(requestListBuff,2048);
+					strcpy(requestListBuff,rl.c_str());
+					fflush(stdout);
+					write(connfd[*id],requestListBuff,strlen(rl.c_str()));
+				}
+				else
+				{
+					fflush(stdout);
+					write(connfd[*id],"There are no request present for your group.\n",strlen("There are no request present for your group.\n"));
+				}
+			}
+			else
+			{
+				fflush(stdout);
+				write(connfd[*id],"You are not logged in.Please login first.\n",strlen("You are not logged in.Please login first.\n"));
+				continue;
+			}
 		}
 		else if(strcmp(buffer,"leg")==0)
 		{
-			cout<<"Leave group called\n";
-			char groupId[10];
-
-			read(connfd[*id],groupId,10);
-			string gId(groupId);
-			auto it = connIdAndUser.find(to_string(connfd[*id]));
-
-			vector<pair<string,string>>::iterator it1;
-
-			for(it1=groupUsers.begin(); it1!=groupUsers.end(); it1++)
+			if(loggedIn[*id]==1)
 			{
-				if(it1->first==gId && it1->second==it->second)
-					break;
+				// cout<<"Leave group called\n";
+				char groupId[10];
+
+				read(connfd[*id],groupId,10);
+				string gId(groupId);
+				auto it = connIdAndUser.find(to_string(connfd[*id]));
+
+				vector<pair<string,string>>::iterator it1;
+				int flag=0;
+				for(it1=groupUsers.begin(); it1!=groupUsers.end(); it1++)
+				{
+					if(it1->first==gId && it1->second==it->second)
+					{
+						flag=1;
+						break;
+					}
+				}
+				if(flag==1)
+				{
+					groupUsers.erase(it1);
+					fflush(stdout);
+					write(connfd[*id],"You have successfully left the group.\n",strlen("You have successfully left the group.\n"));
+				}
+				else
+				{
+					fflush(stdout);
+					write(connfd[*id],"You have not joined this group.\n",strlen("You have not joined this group.\n"));
+				}
 			}
-
-			groupUsers.erase(it1);
-
-			write(connfd[*id],"You have successfully left the group.\n",strlen("You have successfully left the group.\n"));
+			else
+			{
+				fflush(stdout);
+				write(connfd[*id],"You are not logged in.Please login first.\n",strlen("You are not logged in.Please login first.\n"));
+				continue;
+			}
 
 		}
 		else if(strcmp(buffer,"uf")==0)
 		{
-			cout<<"Upload file called\n";
+			// cout<<"Upload file called\n";
 			char fileInfoBuff[1024];
 			read(connfd[*id],fileInfoBuff,1024);
 
 			string fl(fileInfoBuff);
-			cout<<"Recieved string is "<<fl<<endl;
+			// cout<<"Recieved string is "<<fl<<endl;
 			fileTokenizer(fl);
 
-			cout<<"Tokens of file : "<<fileInfo[0]<<"-"<<fileInfo[1]<<"-"<<fileInfo[2]<<"-"<<fileInfo[3]<<endl;
+			// cout<<"Tokens of file : "<<fileInfo[0]<<"-"<<fileInfo[1]<<"-"<<fileInfo[2]<<"-"<<fileInfo[3]<<endl;
 			struct file f = newFile();
-
+			// cout<<"struct object created";
+			auto it = connIdAndUser.find(to_string(connfd[*id]));
+			
 			f.name=fileInfo[0];
 			f.gid=fileInfo[1];
 			f.hashValue=fileInfo[2];
 			f.fileSize =stoi(fileInfo[3]);
+			f.userID=it->second;
 			
 			files.push_back(f);
-
+			// cout<<"File pushed successfully.\n";
 			write(connfd[*id],"File is successfully uploaded.\n",strlen("File is successfully uploaded.\n"));
 
 		}
+		else if(strcmp(buffer,"df")==0)
+		{
+			char fileINFO[500];
+			char gId[10];
+			char fileName[100];
+			char destPath[50];
+			string fInfo = "";
+			
+			read(connfd[*id],gId,10);
+			read(connfd[*id],fileName,100);
+			
+			string grid(gId);
+			string fname(fileName);
 
+			// Find userID of requesting user from connection id
+			auto it = connIdAndUser.find(to_string(connfd[*id]));
+
+			vector<pair<string,string>>::iterator it1;
+			int flag=0;
+			cout<<"Request for file :"<<fileName<<endl;
+			
+			// Check Whether requesting user belongs to that group
+			for(it1=groupUsers.begin(); it1!=groupUsers.end(); it1++)
+			{
+				if(it1->first==grid && it1->second==it->second)
+				{
+					flag=1;
+					break;
+				}			
+			}
+
+			cout<<"file is requested by user "<<it->first<<endl;
+
+			// if(flag==1)
+			{
+				vector<struct file>::iterator it4;
+				// Find userID of user having the requested file
+				for(it4=files.begin(); it4!=files.end(); it4++)
+				{
+					if((*it4).name==fname && (*it4).gid==grid)
+						break;
+				}
+				fflush(stdout);
+				// write(connfd[*id],"1",2);
+				string fi ="";
+				fi+= (*it4).userID;
+				write(connfd[*id],fi.c_str(),strlen(fi.c_str()));
+				// cout<<"File Owner is "<<(*it4).userID<<endl;
+				// char fo[10]={0};
+				// strcpy(fo,((*it4).userID).c_str());
+				// cout<<"File Owner "<<fo<<endl;
+				
+				// // fflush(stdout);
+				
+				// write(connfd[*id],fo,strlen(fo));
+				fflush(stdout);
+				string fs="";
+				fs=to_string((*it4).fileSize);
+				
+				
+				write(connfd[*id],fs.c_str(),strlen(fs.c_str()));
+				fflush(stdout);
+				write(connfd[*id],((*it4).hashValue).c_str(),strlen(((*it4).hashValue).c_str()));
+			}
+			// else 
+			// {
+			// 	fflush(stdout);
+			// 	write(connfd[*id],"0",2);
+			// 	write(connfd[*id],"You are not part of the group.\n",strlen("You are not part of the group.\n"));
+			// }
+		}
 		
 		
 		cout<<"Users logged in are : "<<endl;
@@ -369,11 +565,11 @@ void *readd(void *arg)
 		for(it=user.begin(); it!=user.end(); it++)
 			cout<<it->first<<endl;
 
-		cout<<"Groups are : "<<endl;
-		map<string,string>::iterator it1;
+		// cout<<"Groups are : "<<endl;
+		// map<string,string>::iterator it1;
 		
-		for(auto it1=group.begin(); it1!=group.end(); it1++)
-			cout<<it1->first<<" "<<it1->second<<endl;
+		// for(auto it1=group.begin(); it1!=group.end(); it1++)
+		// 	cout<<it1->first<<" "<<it1->second<<endl;
 
 		cout<<"Request are : "<<endl;
 		vector<pair<string,string>>::iterator it2;
@@ -381,17 +577,22 @@ void *readd(void *arg)
 		for(it2=requestList.begin(); it2!=requestList.end(); it2++)
 			cout<<it2->first<<" : "<<it2->second<<endl;
 
-		cout<<"Connection id and users are : "<<endl;
-		map<string,string>::iterator it3;
+		// cout<<"Connection id and users are : "<<endl;
+		// map<string,string>::iterator it3;
 		
-		for(it3=connIdAndUser.begin(); it3!=connIdAndUser.end(); it3++)
-			cout<<it3->first<<" : "<<it3->second<<endl;
+		// for(it3=connIdAndUser.begin(); it3!=connIdAndUser.end(); it3++)
+		// 	cout<<it3->first<<" : "<<it3->second<<endl;
+
+		// cout<<"Groups and Users are : "<<endl;
+		// vector<pair<string,string>>::iterator it5;
+		// for(it5=groupUsers.begin(); it5!=groupUsers.end(); it5++)
+		// 	cout<<it5->first<<" : "<<it5->second<<endl;
 
 		cout<<"File details : \n";
 		vector<struct file>::iterator it4;
 
 		for(it4=files.begin(); it4!=files.end(); it4++)
-			cout<<(*it4).name<<endl;
+			cout<<(*it4).name<<" "<<(*it4).userID<<endl;
 
 	}
 }
@@ -429,7 +630,7 @@ int main(int argc, char *argv[])
 	int i=0;
 
 
-	while(i<2)
+	while(1)
 	{
 		connfd[i] = accept(sockfd,(struct sockaddr *)&client_address,&clilen);
 		cout<<"Connection id of client is"<<connfd[i]<<endl;
